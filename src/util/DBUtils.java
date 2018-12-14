e60032d2eb25e6d1cbf64675090ec1e5aa25a740
lojn <786824023>
2018-12-14 23:46:14 +0800

    test

--- src/util/DBUtils.java
+++ src/util/DBUtils.java
@@ -1,17 +1,43 @@
 package util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class DBUtils {
     public static Connection getConnection() {
-        Connection connection = null;
+        Connection conn = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/innerworld", "root", "root");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/innerworld", "root", "root");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return connection;
+        return conn;
+    }
+
+    public static void release(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                if (pstmt != null) {
+                    try {
+                        pstmt.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (rs != null) {
+                            try {
+                                rs.close();
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
