2f2a04ad7f5f40b8b1c88655cbcaaa393d5a5d9c
lojn <786824023>
2018-12-15 01:00:03 +0800

    test

--- src/util/DBUtils.java
+++ src/util/DBUtils.java
@@ -1,11 +1,13 @@
 package util;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBUtils {
     private static String driver;
@@ -14,14 +16,24 @@ public class DBUtils {
     private static String password;
 
     static {
-        InputStream is = this.getClass().getResourceAsStream("/..")
+        InputStream is = DBUtils.class.getResourceAsStream("/jdbc.properties");
+        Properties pop = new Properties();
+        try {
+            pop.load(is);
+            driver=pop.getProperty("driver");
+            url=pop.getProperty("url");
+            username=pop.getProperty("username");
+            password=pop.getProperty("password");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static Connection getConnection() {
         Connection conn = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/innerworld", "root", "root");
+            Class.forName(driver);
+            conn = DriverManager.getConnection(driver,username,password);
         } catch (Exception e) {
             e.printStackTrace();
         }
