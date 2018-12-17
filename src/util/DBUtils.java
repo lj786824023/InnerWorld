7f0f87a998ce33bca3641513d5d99bba6c9c2f9f
lojn <786824023>
2018-12-17 12:44:23 +0800

    DBSource

--- src/util/DBUtils.java
+++ src/util/DBUtils.java
@@ -17,7 +17,7 @@ public class DBUtils {
 
     static {
         try {
-            InputStream is = DBUtils.class.getResourceAsStream("/jdbc.properties");
+            InputStream is = DBUtils.class.getResourceAsStream("/util/jdbc.properties");
             Properties pop = new Properties();
             pop.load(is);
             driver = pop.getProperty("jdbc.driver");
